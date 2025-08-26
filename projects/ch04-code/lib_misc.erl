37562b03e364d94e870779e394c4b8cfb874d46b
Hector PENG <unisko # gmail.com>
2025-08-26 10:25:02 +0800

    Finished 'src/part-ii/Ch04-modules_and_functions.md'.

--- projects/ch04-code/lib_misc.erl
+++ projects/ch04-code/lib_misc.erl
@@ -6,7 +6,8 @@
          perms/1,
          max/2,
          filter/2,
-         odds_and_evens/1
+         odds_and_evens/1,
+         odds_and_evens2/1
         ]).
 
 for(Max, Max, F) -> [F(Max)];
@@ -49,3 +50,15 @@ odds_and_evens(L) ->
     Odds  = [X || X <- L, (X rem 2) =:= 1],
     Evens = [X || X <- L, (X rem 2) =:= 0],
     {Odds, Evens}.
+
+
+odds_and_evens2(L) ->
+    odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) ->
+    {lists:reverse(Odds), lists:reverse(Evens)}.
