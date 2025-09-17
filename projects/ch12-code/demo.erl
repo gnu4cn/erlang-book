72bc1b8e23856550a8ab424a1649d5ef2ee65b85
Hector PENG <unisko # gmail.com>
2025-09-17 13:13:29 +0800

    Updated 'src/part-ii/Ch12-concurrent_programming.md'.

--- projects/ch12-code/demo.erl
+++ projects/ch12-code/demo.erl
@@ -25,3 +25,30 @@ receive
 after Time ->
           Expressions
 end
+
+
+receive
+    Pattern1 [when Guard1] ->
+        Expressions1;
+    Pattern2 [when Guard2] ->
+        Expressions2;
+    ...
+after
+    Time ->
+        ExpressionsTimeout
+end
+
+
+
+loop() ->
+    receive
+        {From, {rectangle, Width, Ht}} ->
+            From ! {self(), Width*Ht},
+            loop(),
+            someOtherFunc();
+        {From, {circle, R}} ->
+            From ! {self(), 3.14159 * R * R},
+            loop();
+            ...
+    end
+end
