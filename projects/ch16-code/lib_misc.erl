9972b20e7e7bd0452a2b3a502f8a0b5e983b0353
Hector PENG <unisko@gmail.com>
2025-10-05 15:59:34 +0800

    Updated 'src/part-iv/Ch16-programming_with_files.md'.

--- projects/ch16-code/lib_misc.erl
+++ projects/ch16-code/lib_misc.erl
@@ -1,5 +1,6 @@
 -module(lib_misc).
 -export([
+         unconsult/2,
          dump/2,
          consult/1,
          for/3,
@@ -135,4 +136,14 @@ consult1(S) ->
         Error      -> Error
     end.
 
-dump() -> .
+dump(File, Term) ->
+    Out = File ++ ".tmp",
+    io:format("** dumping to ~s~n", [Out]),
+    {ok, S} = file:open(Out, [write]),
+    io:format(S, "~p.~n", [Term]),
+    file:close(S).
+
+unconsult(File, L) ->
+    {ok, S} = file:open(File, write),
+    lists:foreach(fun(X) -> io:format(S, "~p~n", [X]) end, L),
+    file:close(S).
