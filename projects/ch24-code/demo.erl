57c05edf1fbd2625faf4b73ea5d7049c95440343
Hector PENG <unisko@gmail.com>
2025-10-29 13:37:34 +0800

    Started 'src/part-iv/Ch24-programming_idioms.md'.

---
+++ projects/ch24-code/demo.erl
@@ -0,0 +1,11 @@
+web_server(Client) ->
+    receive
+        {Client, {get, Page}} ->
+            case file:read(Page) of
+                {ok, Bin} ->
+                    Client ! {self(), {data, Bin}};
+                {error, _} ->
+                    Client ! {self(), error}
+            end,
+            web_server(Client)
+    end.
